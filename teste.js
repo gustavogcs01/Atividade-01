6cb1e12fb901493da0237dcdd7689e6dc3c6c7da
gustavogcs01 <124313875+gustavogcs01@users.noreply.github.com>
2023-04-04 21:24:22 -0300

    Add files via upload

---
+++ teste.js
@@ -0,0 +1,62 @@
+var frase = "Aprendendo Javascript";
+console.log(frase)
+
+var numero = 1100;
+var zero = 0;
+var numeroNegativo = -250;
+
+var numeroReal = 25.65;
+var pi = 3.1415;
+var realNegativo = -19.13;
+var realZero = 10.0;
+
+function returnGame(){
+ var game = 'Free Fire';
+ if(game ==='Free Fire'){
+    var game = 'Valorant';
+console.log('Dentro do IF: ' + game);
+}
+console.log('Fora do IF: ' + game);
+} 
+returnGame()
+
+function returnGame(){
+    let game = 'Free Fire';
+    if(game ==='Free Fire'){
+       let game = 'Valorant';
+console.log('Dentro do IF: ' + game);
+}
+console.log('Fora do IF: ' + game);
+} 
+returnGame()
+
+const USER_ADMIN = { name:'Erick Ramos', 
+login:'root'
+};
+USER_ADMIN.login='admin';
+console.log(USER_ADMIN);
+
+var n1 = parseInt (1);
+var n2 = parseInt (2);
+var soma = n1 + n2;
+console.log(soma)
+
+var n1 = parseInt (1);
+var n2 = parseInt (2);
+var soma = n1 - n2;
+console.log(soma)
+
+var n1 = parseInt (10);
+var n2 = parseInt (2);
+var soma = n1 / n2;
+console.log(soma)
+
+var n1 = parseInt (1);
+var n2 = parseInt (2);
+var soma = n1 - n2;
+console.log(soma)
+
+
+
+
+
